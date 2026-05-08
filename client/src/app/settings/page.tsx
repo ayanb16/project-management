5d0901ebafb3be24ac6eb969b0867ccc8fdc4125
ayan banerjee <ayanb.ctc@gmail.com>
2026-05-08 21:17:04 +0530

    init

--- client/src/app/settings/page.tsx
+++ client/src/app/settings/page.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const Settings = () => {
   const userSettings = {
-    username: "johndoe",
-    email: "john.doe@example.com",
+    username: "ayanb",
+    email: "ayanb.dev@gmail.com",
     teamName: "Development Team",
     roleName: "Developer",
   };
